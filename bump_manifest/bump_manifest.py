3fc83ae586258f9b5ccfc4f889b4e67811385bc8
SirLich <lrkoehler77@gmail.com>
2021-10-13 00:16:36 +0200

    Fix version number

--- bump_manifest/bump_manifest.py
+++ bump_manifest/bump_manifest.py
@@ -31,7 +31,7 @@ def get_version() -> str:
         data = json.load(f)
 
     # Update version file
-    data['version'] = data['version'][-1] + 1
+    data['version'][-1] = data['version'][-1] + 1
     with open(path, 'w') as f:
         json.dump(data, f, indent=4)
 
