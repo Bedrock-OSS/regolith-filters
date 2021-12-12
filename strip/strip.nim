9db1b43705c86d5305e27a88c3fea357277ad586
evilguy50 <robbryan42@gmail.com>
2021-12-12 08:56:50 -0800

    Add files via upload

---
+++ strip/strip.nim
@@ -0,0 +1,25 @@
+import os, re, strutils, json
+ 
+for file in walkDirRec os.getCurrentDir():
+    var newF: string
+    var newL: string
+    if file.match re".*\.json":
+        for line in file.readFile().splitLines():
+            newL = line
+            if line.contains("//"):
+                newL = line.replace("//" & line.split("//")[1], "")
+            newF = newF & newL & "\n"
+        file.writeFile(newF.parseJson().pretty())
+        echo "formatted " & file.splitFile().name & ".json"
+    if file.match re".*\.lang":
+        for line in file.readFile().splitLines():
+            newL = line
+            if line.startsWith("##") == false and line != "":
+                newF = newF & newL & "\n"
+        file.writeFile(newF)
+    if file.match re".*\.mcfunction":
+        for line in file.readFile().splitLines():
+            newL = line
+            if line.startsWith("#") == false and line != "":
+                newF = newF & newL & "\n"
+        file.writeFile(newF)
