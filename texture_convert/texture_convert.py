11ac238ad609601ed50694ea1fefbf19260c28bd
Nusiq <nusiq@outlook.com>
2024-10-13 22:25:41 +0200

    texture_convert: Clean up the code.

--- texture_convert/texture_convert.py
+++ texture_convert/texture_convert.py
@@ -1,55 +1,55 @@
-from pathlib import Path  # Import the Path class for cleaner path handling.
-import layeredimage.io  # Import the layeredimage library for handling layered image formats like PSD, PDN, XCF.
-import zipfile  # Import the zipfile module to handle zip files (used for .kra format images).
-from PIL import Image, ImageSequence# Import PIL for gif conversion
+from pathlib import Path
+import layeredimage.io
+import zipfile
+from PIL import Image, ImageSequence
 
 def convert_kra(imgpath: Path):
-    with zipfile.ZipFile(imgpath) as z:  # Open the .kra file as a zip archive.
-        with imgpath.with_suffix(".png").open(
-            "wb"
-        ) as f2:  # Create the output PNG file.
-            f2.write(z.read("preview.png"))  # Write the preview image to the PNG file.
-
+    '''
+    Converts a Krita file to a PNG.
+    '''
+    # Krita files are actually zip files
+    with zipfile.ZipFile(imgpath) as z:
+        with imgpath.with_suffix(".png").open("wb") as f2:
+            f2.write(z.read("preview.png"))
 
 def convert_layered(imgpath: Path):
-    img = layeredimage.io.openLayerImage(imgpath)  # Open the layered image.
-    img.getFlattenLayers().save(imgpath.with_suffix(".png"))  # Flatten and save as PNG.
+    '''
+    Converts a layered image format (like .pdn, .xcf or .psd) to a flattened
+    PNG.
+    '''
+    img = layeredimage.io.openLayerImage(imgpath)
+    img.getFlattenLayers().save(imgpath.with_suffix(".png"))
 
 def convert_gif(imgpath: Path):
+    '''
+    Converts a GIF file to a PNG sprite sheet.
+    '''
     with Image.open(imgpath) as img:
         frames = ImageSequence.all_frames(img)
-        
+
     frame_width = frames[0].width
     frame_height = frames[0].height
     atlas_height = frame_height * len(frames)
-    
+
     # Create canvas for atlas
     atlas = Image.new("RGBA", (frame_width, atlas_height))
-    
+
     for index, frame in enumerate(frames):
         atlas.paste(frame, (0, index * frame_height))
     atlas.save(imgpath.with_suffix(".png"))
-            
-    
+
+
 # EXECUTE SCRIPT
 if __name__ == "__main__":
-
-    # Use rglob to recursively search for files in all subdirectories.
     for imgpath in Path(".").rglob("*"):
-
-        # Check if the current item is a file.
-        if imgpath.is_file():
-
-            # Handle Photoshop, paint.net and XCF file
-            if imgpath.suffix in [".pdn", ".xcf", ".psd"]:
-                convert_layered(imgpath)  # Convert layered formats to png
-                imgpath.unlink()  # Delete the original file.
-
-            # Handle Krita file
-            if imgpath.suffix == ".kra":
-                convert_kra(imgpath)  # Convert .kra to png
-                imgpath.unlink()  # Delete the original file.
-            # Handle GIF file
-            if imgpath.suffix == ".gif":
-                convert_gif(imgpath)  # Convert .gif to png
-                imgpath.unlink()  # Delete the original file.
+        if not imgpath.is_file():
+            continue
+        if imgpath.suffix in [".pdn", ".xcf", ".psd"]:
+            convert_layered(imgpath)
+            imgpath.unlink()
+        elif imgpath.suffix == ".kra":
+            convert_kra(imgpath)
+            imgpath.unlink()
+        elif imgpath.suffix == ".gif":
+            convert_gif(imgpath)
+            imgpath.unlink()
